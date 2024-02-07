5517eb772cba37bea77b40d6d8a71361d315bf38
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-07 20:17:37 +0300

    01 - tests done

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,5 +1,15 @@
 const sentense = 'Hello my friends!'
 
-function splitIntowords(sentense:string){
-    return ['hello', 'my', 'friends']
+export function splitIntoWords(sentense: string) {
+    const words = sentense.toLowerCase().split(' ')
+    return words.filter(w => w !== '-').map(w => w.replace('!', ''))
 }
+
+export function sum(a: number, b: number) {
+    return a + b
+}
+
+export function mult(a: number, b: number) {
+    return a * b
+}
+
